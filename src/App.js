8c691d4f4101c66cff70fcf1d330fc7affeac404
omkareshwar9849 <omkar.varada9849@gmail.com>
2023-04-22 13:10:03 +0530

    video 11 completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
  
 function App() {
   return (
@@ -10,8 +10,8 @@ function App() {
     {/* <Navbar/> */}
     <Navbar title="TextUtils" />
     <div className="container my-3">
-    {/* <TextForm heading="Enter the text to analyze below"/> */}
-    <About/>
+    <TextForm heading="Enter the text to analyze below"/>
+    {/* <About/> */}
     </div>
     </> 
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -56,6 +56,25 @@ export default function TextForm(props) {
         setrWord(event.target.value)
     }
 
+    const handleRClear = () => {
+        setfWord("");
+        setrWord("");
+    }
+
+    const handleCopy = () => {
+        console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
+
     const [text, setText] = useState(''); 
     const [tempText,setTempText] = useState('');
     const [fWord, setfWord] = useState("");
@@ -74,6 +93,8 @@ export default function TextForm(props) {
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
                 <button className="btn btn-primary mx-1" onClick={speak}> Speak </button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
 
             <div className="container my-3 ">
@@ -83,6 +104,7 @@ export default function TextForm(props) {
                     Replace word :
                     <input onChange={handlerWord} className='mx-2' type="text" value={rWord} />
                     <button className="btn btn-primary mx-1" onClick={handlereplace}> Replace </button>
+                    <button className="btn btn-primary mx-1" onClick={handleRClear}> Clear </button>
                 </div>
                 <h2>Yout text summary</h2>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
