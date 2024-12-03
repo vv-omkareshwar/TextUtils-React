92f06e653f9dd01830d6a8930510a9326bd61a35
vv-omkareshwar <varadha.omkareshwar@techolution.com>
2024-12-03 22:53:44 +0530

    Initial commit on orphan branch

---
+++ TextUtils-React-7d8f9/src/App.js
@@ -0,0 +1,5 @@
+d that you want me to convert the file at the path TextUtils-React-7d8f9/src/App.js. However, you haven't provided the content of this file. Without the actual code to convert, I can't perform the conversion task.
+
+To proceed with the conversion, I would need the content of the App.js file. Once you provide the file content, I can analyze it, convert it according to the specified requirements, and provide the converted code along with any necessary dependencies and a summary.
+
+Could you please provide the content of the App.js file? Once I have that, I'll be able to perform the conversion task as requested
---
+++ TextUtils-React-7d8f9/tests/setupTests.js
@@ -0,0 +1,12 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+// This file is a setup file for Jest tests.
+// It imports the '@testing-library/jest-dom' library, which extends Jest's expect
+// function with methods for testing DOM elements.
+
+// No additional setup is required for this project.
+// If you need to add more global test setup, you can do so here.
